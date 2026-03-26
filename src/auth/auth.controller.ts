bf674d7b5f687e7ed2dcf03e61cbf7c4f279a87b
Hashim <syedhashim721@gmail.com>
2026-03-26 13:16:05 +0530

    first mewooow

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+    authService: AuthService;
+    constructor( authService: AuthService) {
+        this.authService = authService;
+    }
+    @Post('/register')
+    registerUser(){
+        const result = this.authService.registerUser();
+        return result;
+    }
+}
