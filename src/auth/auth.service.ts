881effdda6f459820bf747e1862e7ba2d7e006f0
Hashim <syedhashim721@gmail.com>
2026-03-26 13:16:05 +0530

    first mewooow

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+    constructor(private userService: UserService) {}
+    registerUser(){  
+ 
+
+
+       this.userService.createUser();
+        return {message: 'User registered successfully'}
+    }
+}
+ 
